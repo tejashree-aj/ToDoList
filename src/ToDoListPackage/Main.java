6aba8fd941543a974dcc129b9a1d0acbb0bb8539
tejashreejagdale <dr.tejashree.aj@gmail.com>
2019-10-11 00:32:42 +0200

    Initial Commit ToDo List

--- src/ToDoListPackage/Main.java
+++ src/ToDoListPackage/Main.java
@@ -53,7 +53,6 @@ public class Main {
                     System.out.println("Save & Quit");
                     in.close();
                     break;
-
             }
 
         } while (opChoice != 0);
